239ab7c6fec0f0b88cbe206b69e6dd0890d5b87a
sam-hur <83226062+sam-hur@users.noreply.github.com>
2022-04-11 19:01:10 +0200

    fixed file structure error

--- samhurenkamp_react_app/development/frontend/index.js
+++ samhurenkamp_react_app/development/frontend/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Table from "../components/table/table";
+import Table from "../backend/components/table/table";
 import './styles/stylesheet.scss'
 
 
